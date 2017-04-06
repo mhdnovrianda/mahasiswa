df4760e0a2b428cbe7d74b1e519dbd5d9c78087c
Muhammad Novrianda <mhdnovrianda@gmail.com>
2017-04-06 08:41:24 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,35 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+class FibonacciExample1{ 
+    
+    public static void main(String args[])  
+    {    
+        Scanner sc = new Scanner (System.in);
+        int n1=0,n2=1,n3,i;
+        System.out.print("Masukkan Angka : ");
+        int count = sc.nextInt();
+        System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+        for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+        {    
+            n3=n1+n2;    
+            System.out.print(" "+n3);    
+            n1=n2;    
+            n2=n3;    
+        }    
+    }
+} 
+    
+
